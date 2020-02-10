a949c83856ee2b737680a217a411d924c94a25d8
Karan Kumar <nailedITnerd@gmail.com>
2020-02-10 22:28:33 +0530

    html done

--- todo/urls.py
+++ todo/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('edit/<int:pk>', views.edit, name='edit'),
+    path('delete/<int:pk>', views.delete, name='delete')
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse 
+from .models import Task
+from .forms import TaskForm
 
 # Create your views here.
 
 def home(request):
-	return render(request, 'home.html', {}) 
 
+	#model initializer
+	tasks = Task.objects.all()
+
+	if request.method == 'POST':
+		#add values to form if method == POST
+		form = TaskForm(request.POST or none)
+		if form.is_valid():
+			form.save()
+		return redirect('home')
+
+	#form initializer
+	form = TaskForm() 
+	context = {
+		'tasks': tasks,
+		'form' : form,
+		}
+	return render(request, 'home.html', context) 
+
+def edit(request, pk):
+	
+	#model initializer --> specific indexed
+	task = Task.objects.get(id=pk)
+
+	#form initializer --> specific indexed
+	#this will pre-fill the form 
+	form = TaskForm(instance=task)
+
+	if request.method == 'POST':
+		form = TaskForm(request.POST, instance=task)
+		if form.is_valid():
+			form.save()
+		return redirect('home')
+
+	context = {
+		'task': task,
+		'form': form,
+		}
+	return render(request, 'edit.html', context)
+
+def delete(request, pk):
+
+	#model initializer
+	task = Task.objects.get(id=pk)
+	if request.method == 'POST':
+			task.delete()
+			return redirect('home')
+	context = {
+		'tasks':task,
+	}
+	return render(request, 'delete.html', context)
